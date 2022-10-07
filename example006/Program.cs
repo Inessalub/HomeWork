6a0ee067fff50ad9f6b5644198a660cf298cc4ef
unknown <inessalub@gmail.com>
2022-10-07 20:26:42 +0400

    example006

---
+++ example006/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное  число ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+
+int Number(int b)
+{
+    return b % 10;
+}
+Console.WriteLine("Последняя цифра " + Number(a));
